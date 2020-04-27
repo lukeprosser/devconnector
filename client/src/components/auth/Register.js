4c742f970ca8f1ba49f6e58d06c30e317f4fd5f0
Luke Prosser <luke.prosser@cloud-iq.com>
2020-04-27 11:54:41 +0100

    Add logout action and update navbar links

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -53,7 +53,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
             name='name'
             value={name}
             onChange={(e) => onChange(e)}
-            // required
+            required
           />
         </div>
         <div className='form-group'>
@@ -63,7 +63,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
             name='email'
             value={email}
             onChange={(e) => onChange(e)}
-            // required
+            required
           />
           <small className='form-text'>
             This site uses Gravatar so if you want a profile image, use a
@@ -77,7 +77,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
             name='password'
             value={password}
             onChange={(e) => onChange(e)}
-            // minLength='6'
+            minLength='6'
           />
         </div>
         <div className='form-group'>
@@ -87,7 +87,7 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
             name='password2'
             value={password2}
             onChange={(e) => onChange(e)}
-            // minLength='6'
+            minLength='6'
           />
         </div>
         <input type='submit' className='btn btn-primary' value='Register' />
