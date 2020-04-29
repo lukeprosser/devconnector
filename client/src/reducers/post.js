9b99bf2c0d57bc9739ce8d5263c88a7f124964a4
Luke Prosser <luke.prosser@cloud-iq.com>
2020-04-29 16:33:15 +0100

    Post reducer & action | Post & post item component | Post like & unlike

---
+++ client/src/reducers/post.js
@@ -0,0 +1,40 @@
+/** @format */
+
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: null,
+  loading: true,
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        loading: false,
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          // Check if post matches updated post and either update likes array or return regular post
+          post._id === payload.postId ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
